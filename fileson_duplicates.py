751eb34bbcb7f36f23c424529e30a2ae5c82f77b
Joonas Pihlajamaa <joonas.pihlajamaa@iki.fi>
2021-02-15 21:11:45 +0200

    Cleaned up fileson_duplicates

--- fileson_duplicates.py
+++ fileson_duplicates.py
@@ -10,20 +10,15 @@ minsize = int(args.size_min.replace('G', '000M').replace('M', '000k').replace('k
 
 fs = fileson.load(args.dbfile)
 files = [f for f in fileson.filelist(fs) if f['size'] >= minsize]
-
-if not files:
-    print('No files.')
-    exit(0)
-
 checksum = fs['checksum']
-if checksum == 'none':
-    print('No checksum!')
-    exit(0)
 
-csums = defaultdict(list)
-for f in files: csums[f[checksum]].append(f)
+if not files: print('No files.')
+if checksum == 'none': print('No checksum!')
+else:
+    csums = defaultdict(list)
+    for f in files: csums[f[checksum]].append(f)
 
-for csum in [cs for cs in csums if len(csums[cs]) > 1]:
-    files = csums[csum]
-    print(files[0]['size'], 'bytes', checksum, 'checksum', csum, ':')
-    for f in csums[csum]: print(os.sep.join(fileson.path(f)))
+    for csum in [cs for cs in csums if len(csums[cs]) > 1]:
+        files = csums[csum]
+        print(files[0]['size'], 'bytes', checksum, 'checksum', csum)
+        for f in csums[csum]: print(os.sep.join(fileson.path(f)))
