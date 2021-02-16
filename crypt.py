18ba48c6ac37d69b1ad08c660fb6c888a7193a22
Joonas Pihlajamaa <joonas.pihlajamaa@iki.fi>
2021-02-16 20:15:16 +0200

    Crypto routines replaced with AESFile wrapper

--- crypt.py
+++ crypt.py
@@ -1,37 +1,23 @@
-import hashlib
 from Crypto.Cipher import AES
 from Crypto.Util import Counter
-from Crypto.Random import get_random_bytes
+import hashlib
 
-def encrypt(infile, outfile, key):
-    ctr = Counter.new(128)
-    obj = AES.new(key, AES.MODE_CTR, counter=ctr)
-    bs = 0
+class AESFile:
+    """VERY simple wrapper for AES CTR file en/decryption.
+    Does the bare minimum, you may get errors if not careful."""
+    @staticmethod
+    def key(passStr, saltStr, iterations=10000):
+        return hashlib.pbkdf2_hmac('sha256', passStr.encode('utf8'),
+            saltStr.encode('utf8'), iterations)
 
-    with open(infile, 'rb') as fin:
-        with open(outfile, 'wb') as fout:
-            while True:
-                data = fin.read(65536)
-                if not data: break
-                fout.write(obj.encrypt(data))
-                bs += len(data)
-
-    return bs
-
-def decrypt(infile, outfile, key):
-    ctr = Counter.new(128)
-    obj = AES.new(key, AES.MODE_CTR, counter=ctr)
-    bs = 0
-
-    with open(infile, 'rb') as fin:
-        with open(outfile, 'wb') as fout:
-            while True:
-                data = fin.read(65536)
-                if not data: break
-                fout.write(obj.decrypt(data))
-                bs += len(data)
-
-    return bs
+    def __init__(self, filename, mode, key):
+        ctr = Counter.new(128)
+        self.obj = AES.new(key, AES.MODE_CTR, counter=ctr)
+        if mode in ('wb', 'rb'): self.fp = open(filename, mode)
+        else: raise RuntimeError('Only rb and wb modes supported!')
+    def write(self, data): return self.fp.write(self.obj.encrypt(data))
+    def read(self, size=-1): return self.obj.decrypt(self.fp.read(size))
+    def close(self): self.fp.close()
 
 if __name__ == "__main__":
     import argparse, time
@@ -45,14 +31,26 @@ if __name__ == "__main__":
     parser.add_argument('-i', '--iterations', type=int, default=100000,
             help='PBKDF2 iterations (default 100000)')
     args = parser.parse_args()
-    
+    key = AESFile.key(args.password, args.salt, args.iterations)
 
-    key = hashlib.pbkdf2_hmac('sha256', args.password.encode('utf8'),
-            args.salt.encode('utf8'), args.iterations)
+    startTime, bs = time.time(), 0
 
-    startTime = time.time()
-    bs = (encrypt if args.mode == 'encrypt' else decrypt) \
-            (args.input, args.output, key)
+    if args.mode == 'encrypt':
+        infile = open(args.input, 'rb')
+        outfile = AESFile(args.output, 'wb', key)
+    else:
+        infile = AESFile(args.input, 'rb', key)
+        outfile = open(args.output, 'wb')
+
+    while True:
+        data = infile.read(65536)
+        if not data: break
+        outfile.write(data)
+        bs += len(data)
+
+    infile.close()
+    outfile.close()
+
     secs = time.time() - startTime
 
     print(f'{bs} bytes {args.mode}ed', 'in %.1f seconds, %.2f GB/s' %
