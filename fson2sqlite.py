0d9d155327359a2705e4f93c55ffa61ebf1fb310
Joonas Pihlajamaa <joonas.pihlajamaa@iki.fi>
2022-04-02 11:16:51 +0300

    Change type to sqlite3 dump
    Former-commit-id: ea533d9c9ff862f3e08070c55a7b50e42057996a

--- fson2sqlite.py
+++ fson2sqlite.py
@@ -1,10 +1,10 @@
 import json, sys, sqlite3
 
 if len(sys.argv) < 3:
-    print('Usage: fson2sqlite [file.fson] [sqlite.db]')
+    print('Usage: fson2sqlite [sqlite.db] [file.fson] ...')
     exit(1)
 
-con = sqlite3.connect(sys.argv[2])#, isolation_level=None)
+con = sqlite3.connect(sys.argv[1])#, isolation_level=None)
 cur = con.cursor()
 
 cur.execute('''CREATE TABLE IF NOT EXISTS entries (
@@ -20,47 +20,58 @@ cur.execute('''CREATE TABLE IF NOT EXISTS scans (
 
 cur.execute('CREATE INDEX IF NOT EXISTS scan_entry_idx ON scans(entry_id)')
 
+# op = 0 for deletion, 1 for modification, 2 for creation
+# modified_gmt NULL for deletions
+# size NULL for directories
+# sha1 only for files
 cur.execute('''CREATE TABLE IF NOT EXISTS files (
     scan_id INTEGER NOT NULL,
     filename TEXT NOT NULL,
+    op INTEGER NOT NULL,
     modified_gmt TEXT DEFAULT NULL,
     size INTEGER DEFAULT NULL,
     sha1 TEXT DEFAULT NULL
 )''')
 
-name = sys.argv[1] #input('Name of entry to add: ')
+for name in sys.argv[2:]:
+    cur.execute('INSERT INTO entries (name) VALUES (?)', (name,))
+    entry_id = cur.lastrowid
+    print('Created entry', entry_id)
+    
+    current = {} # Current state
 
-cur.execute('INSERT INTO entries (name) VALUES (?)', (name,))
-entry_id = cur.lastrowid
-print('Created entry', entry_id)
+    with open(name, 'r', encoding='utf8') as fin:
+        inmeta = False
+        meta = {}
+        for l in fin.readlines():
+            t = json.loads(l)
+            if t[0][0] == ':':
+                if not inmeta: meta = {}
+                inmeta = True
+                meta[t[0]] = t[1]
+            else:
+                if inmeta:
+                    inmeta = False
+                    print(meta)
+                    cur.execute('''INSERT INTO scans (entry_id, date_gmt, folder)
+                            VALUES (?, ?, ?)''',
+                            (entry_id, meta.get(':date_gmt:', ''), meta.get(':directory:', '')))
+                    scan_id = cur.lastrowid
 
-with open(sys.argv[1], 'r', encoding='utf8') as fin:
-    inmeta = False
-    meta = {}
-    for l in fin.readlines():
-        t = json.loads(l)
-        if t[0][0] == ':':
-            if not inmeta: meta = {}
-            inmeta = True
-            meta[t[0]] = t[1]
-        else:
-            if inmeta:
-                inmeta = False
-                print(meta)
-                cur.execute('''INSERT INTO scans (entry_id, date_gmt, folder)
-                        VALUES (?, ?, ?)''',
-                        (entry_id, meta.get(':date_gmt:', ''), meta.get(':directory:', '')))
-                scan_id = cur.lastrowid
-
-            if len(t)==1: # deletion
-                cur.execute('''INSERT INTO files (scan_id, filename)
-                        VALUES(?, ?)''', (scan_id, t[0]))
-            elif 'size' in t[1]: # new file
-                cur.execute('''INSERT INTO files (scan_id, filename, modified_gmt, size, sha1)
-                    VALUES(?, ?, ?, ?, ?)''',
-                    (scan_id, t[0], t[1]['modified_gmt'], t[1]['size'], t[1]['sha1']))
-            else: # new folder
-                cur.execute('''INSERT INTO files (scan_id, filename, modified_gmt)
-                        VALUES(?, ?, ?)''', (scan_id, t[0], t[1]['modified_gmt']))
+                fn = t[0]
+                op = 1 if fn in current else 2 # 1 modified 2 new
+                current[fn] = True
+                if fn =='.': pass # skip root creation from "legacy" scans and deletion later on
+                elif len(t)==1: # deletion
+                    del current[fn]
+                    cur.execute('''INSERT INTO files (scan_id, filename, op)
+                            VALUES(?, ?, ?)''', (scan_id, t[0], 0))
+                elif 'size' in t[1]: # new file
+                    cur.execute('''INSERT INTO files (scan_id, filename, modified_gmt, size, sha1, op)
+                        VALUES(?, ?, ?, ?, ?, ?)''',
+                        (scan_id, t[0], t[1]['modified_gmt'], t[1]['size'], t[1]['sha1'], op))
+                else: # new folder
+                    cur.execute('''INSERT INTO files (scan_id, filename, modified_gmt, op)
+                            VALUES(?, ?, ?, ?)''', (scan_id, t[0], t[1]['modified_gmt'], op))
 con.commit()
 con.close()
